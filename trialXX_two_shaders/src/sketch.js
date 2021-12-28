f9e1ea91efdcff8742c29e035c54fee6f9c12591
Satoru Osawa <osawa@takram.com>
2021-12-28 23:02:13 +0900

    Refactoring

--- trialXX_two_shaders/src/sketch.js
+++ trialXX_two_shaders/src/sketch.js
@@ -5,23 +5,15 @@ let bufferArray = []
 let shaderArray = []
 
 export const Sketch = p5 => {
-  let isFirstPGraphics = true
+  let backBufferIsZero = true
 
-  const backBuffer = () => {
-    return bufferArray[isFirstPGraphics ? 0 : 1]
-  }
+  const backBuffer = () => bufferArray[backBufferIsZero ? 0 : 1]
 
-  const frontBuffer = () => {
-    return bufferArray[isFirstPGraphics ? 1 : 0]
-  }
+  const frontBuffer = () => bufferArray[backBufferIsZero ? 1 : 0]
 
-  const backShader = () => {
-    return shaderArray[isFirstPGraphics ? 0 : 1]
-  }
+  const backShader = () => shaderArray[backBufferIsZero ? 0 : 1]
 
-  const frontShader = () => {
-    return shaderArray[isFirstPGraphics ? 1 : 0]
-  }
+  const frontShader = () => shaderArray[backBufferIsZero ? 1 : 0]
 
   p5.preload = () => {
     for (let i = 0; i < 2; i++) {
@@ -43,8 +35,9 @@ export const Sketch = p5 => {
     frontBuffer().shader(frontShader())
     frontShader().setUniform('resolution', [p5.width, p5.height])
     frontShader().setUniform('buffer', backBuffer())
-    frontBuffer().rect(-50, -50, 100, 100)
-    p5.image(frontBuffer(), -50, -50, p5.width, p5.height)
-    isFirstPGraphics = !isFirstPGraphics
+    frontBuffer().rect(-p5.width / 2, -p5.height / 2, p5.width, p5.height)
+    p5.background(255)
+    p5.image(frontBuffer(), -p5.width / 2, -p5.height / 2, p5.width, p5.height)
+    backBufferIsZero = !backBufferIsZero
   }
 }
