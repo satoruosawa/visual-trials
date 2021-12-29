cd90f7c6ca0ad0253a2666270a700fee91cf2cfd
Satoru Osawa <osawa@takram.com>
2021-12-30 01:06:34 +0900

    Add tutorial

--- trial13_p5js/src/sketch.js
+++ trial13_p5js/src/sketch.js
@@ -11,6 +11,7 @@ export const Sketch = p5 => {
   let pGraphics
   const ticker = new Ticker()
   let notoSans
+  let isInteracted = false
 
   p5.preload = () => {
     notoSans = p5.loadFont('assets/NotoSans-Regular.ttf')
@@ -20,19 +21,22 @@ export const Sketch = p5 => {
     canvas = p5.createCanvas(1000, 1000)
     pGraphics = p5.createGraphics(p5.width, p5.height)
     field = new Field(p5, rectSize, p5.width / rectSize, p5.height / rectSize)
+    pGraphics.textFont(notoSans)
+    pGraphics.textSize(600)
+    pGraphics.noStroke()
+    pGraphics.fill(0)
+    pGraphics.textAlign(p5.CENTER, p5.CENTER)
+    p5.textFont(notoSans)
+    p5.textSize(100)
+    p5.textAlign(p5.CENTER, p5.CENTER)
   }
 
   const update = () => {
     ticker.update()
     if (ticker.frameCount % 180 == 0) {
-      pGraphics.textFont(notoSans)
-      pGraphics.textSize(600)
-      pGraphics.background(255)
-      pGraphics.noStroke()
-      pGraphics.fill(0)
-      pGraphics.textAlign(p5.CENTER, p5.CENTER)
       const number = (ticker.frameCount / 180) % 10
-      pGraphics.text(number, p5.width / 2, p5.height / 2)
+      pGraphics.background(255)
+      pGraphics.text(number, p5.width / 2, p5.height / 2 - 100)
       pGraphics.loadPixels()
       const pixelDensity = p5.pixelDensity()
       for (let j = 0; j < pGraphics.height; j += 2) {
@@ -60,9 +64,19 @@ export const Sketch = p5 => {
     update()
     p5.background(255)
     particleSystem.draw()
+    if (!isInteracted) {
+      p5.fill(255, 200)
+      p5.noStroke()
+      p5.rectMode(p5.CENTER)
+      p5.rect(p5.width / 2, p5.height / 2, 500, 200, 20)
+      p5.fill(255, 0, 0)
+      p5.text('Mix here!!', p5.width / 2, p5.height / 2 - 15)
+      p5.rectMode(p5.CORNER)
+    }
   }
 
   p5.mouseMoved = () => {
+    isInteracted = true
     const diffMouse = p5
       .createVector(p5.mouseX - p5.pmouseX, p5.mouseY - p5.pmouseY)
       .mult(2)
@@ -71,6 +85,7 @@ export const Sketch = p5 => {
   }
 
   p5.touchMoved = () => {
+    isInteracted = true
     const diffMouse = p5
       .createVector(p5.mouseX - p5.pmouseX, p5.mouseY - p5.pmouseY)
       .mult(2)
